91715280ef78a9a7b467f21aa8c5257a066d5587
getintern <121359731+getintern@users.noreply.github.com>
2023-10-28 23:47:44 +0330

    remove files

--- src/app/core/i18n.js
+++
@@ -1,7 +0,0 @@
-import i18n from "i18next";
-import Backend from "i18next-http-backend";
-import { initReactI18next } from "react-i18next";
-
-i18n.use(Backend).use(initReactI18next).init({ lng: "fa" });
-
-export default i18n;
