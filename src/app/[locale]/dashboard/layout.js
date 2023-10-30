22bd4a8b24afc7a7dc2b64e0bc217d02fdcd443f
getintern <121359731+getintern@users.noreply.github.com>
2023-10-30 11:41:03 +0330

    Create layout.js

---
+++ src/app/[locale]/dashboard/layout.js
@@ -0,0 +1,7 @@
+import DashboardSidebar from "@/app/components/Layouts/DashboardSidebar";
+
+const DashboardLayout = ({ children }) => {
+  return <DashboardSidebar>{children}</DashboardSidebar>;
+};
+
+export default DashboardLayout;
